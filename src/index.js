d44d5395df438e90fb9c379f0064ade76816fb63
Sebastien <sebastien.wagnieres@heig-vd.ch>
2021-06-23 15:39:28 +0200

    feat: work in progress on data_rockets

--- src/index.js
+++ src/index.js
@@ -7,14 +7,25 @@ import {
   interpolateNumber,
 } from 'd3'
 
-const DATA = [
-  { nom: 'Lausanne', population: 138905 },
-  { nom: 'Yverdon-les-Bains', population: 30143 },
-  { nom: 'Montreux', population: 26574 },
-  { nom: 'Renens', population: 21036 },
-  { nom: 'Nyon', population: 20533 },
-  { nom: 'Vevey', population: 19827 },
-]
+import _data_cores from '/spacex/json/cores.json';
+import _data_launches from '/spacex/json/launches.json';
+import _data_rockets from '/spacex/json/rockets.json';
+
+// const data_cores = _data_cores.map(d =>({
+//   reuse: 
+// }))
+
+
+// const data_cores = _data_cores.map(d =>({
+//   reuse: 
+// }))
+
+
+const data_rockets = _data_rockets.map(d =>({
+  name: d.name,
+  height: d.height_mt,
+  diameter: d.diameter_mt,
+}))
 
 const img = {
   viewBox: '0 0 92.08 61.99',
@@ -28,14 +39,14 @@ const MARGIN_LEFT = 50
 const MARGIN_BOTTOM = 50
 const MARGIN_TOP = 50
 const BAR_HEIGHT = HEIGHT - MARGIN_TOP - MARGIN_BOTTOM
-const BAR_WIDTH = (WIDTH - MARGIN_LEFT) / DATA.length
+const BAR_WIDTH = (WIDTH - MARGIN_LEFT) / data_rockets.length
 
 const svg = select('#fusees')
   .append('svg')
   .attr('viewBox', `0 0 ${WIDTH} ${HEIGHT}`)
 
 const yScale = scaleLinear()
-  .domain([0, max(DATA, d => d.population)])
+  .domain([0, max(data_rockets, d => d.height_mt)])
   .range([BAR_HEIGHT, 0])
 
 
@@ -43,20 +54,20 @@ const g = svg.append('g')
   .attr('transform', `translate(${MARGIN_LEFT}, ${MARGIN_TOP})`)
 
 g.selectAll('rect')
-  .data(DATA)
+  .data(data_rockets)
   .enter()
   .append('rect')
   .attr('x', (d, i) =>  i * BAR_WIDTH)
   .attr('width', BAR_WIDTH - MARGIN)
-  .attr('y', d => yScale(d.population))
-  .attr('height', d => BAR_HEIGHT - yScale(d.population))
+  .attr('y', d => yScale(d.height_mt))
+  .attr('height', d => BAR_HEIGHT - yScale(d.height_mt))
   .attr('fill', 'steelblue')
 
 g.selectAll('text')
-  .data(DATA)
+  .data(data_rockets)
   .enter()
   .append('text')
-  .text(d => d.nom)
+  .text(d => d.name)
   .attr('x', (d, i) =>  i * BAR_WIDTH + BAR_WIDTH / 2)
   .attr('y', BAR_HEIGHT + MARGIN_BOTTOM / 2)
   .attr('text-anchor', 'middle')
@@ -74,13 +85,13 @@ const imagesG = g.append('g')
   .attr('transform', `translate(${-BAR_WIDTH / 2}, -15)`)
 
 imagesG.selectAll('svg')
-  .data(DATA)
+  .data(data_rockets)
   .enter()
   .append('svg')
   .attr('viewBox', img.viewBox)
   .attr('width', BAR_WIDTH - MARGIN)
   .attr('x', (d, i) =>  i * BAR_WIDTH + BAR_WIDTH / 2)
-  .attr('y', d => yScale(d.population) - HEIGHT / 2)
+  .attr('y', d => yScale(d.height_mt) - HEIGHT / 2)
   .append('path')
     .attr('d', img.d)
     .attr('fill', 'steelblue')
